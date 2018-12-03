92a26d8e95019dfe7a82da7de87089ae84656018
Ediz Turcan <ediz.turcan@gmail.com>
2018-12-03 21:10:10 +0100

    fixed default value 1 cell

--- ToolBox/Views/TBDefaultValue1TableViewCell.swift
+++ ToolBox/Views/TBDefaultValue1TableViewCell.swift
@@ -9,11 +9,61 @@
 import UIKit
 
 public class TBDefaultValue1TableViewCell: UITableViewCell {
+
+    public let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 1
+        label.allowsDefaultTighteningForTruncation = false
+        label.backgroundColor = .red
+        return label
+    }()
+    
+    public let detailLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .right
+        label.numberOfLines = 1
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .blue
+        return label
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: .value1, reuseIdentifier: reuseIdentifier)
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        textLabel?.isHidden = true
+        detailTextLabel?.isHidden = true
+        
+        self.contentView.addSubview(titleLabel)
+        self.contentView.addSubview(detailLabel)
+        
+        addConstraints([
+            NSLayoutConstraint(item: titleLabel, attribute: .top, relatedBy: .equal,
+                               toItem: contentView, attribute: .top, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: titleLabel, attribute: .leading, relatedBy: .equal, toItem: contentView,
+                               attribute: .leading, multiplier: 1, constant: .tbSpacingCellHorizontalPadding),
+            NSLayoutConstraint(item: titleLabel, attribute: .bottom, relatedBy: .equal,
+                               toItem: contentView, attribute: .bottom, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: titleLabel, attribute: .trailing, relatedBy: .equal,
+                               toItem: contentView, attribute: .trailing, multiplier: 0.5, constant: 0),
+            NSLayoutConstraint(item: detailLabel, attribute: .top, relatedBy: .equal,
+                               toItem: contentView, attribute: .top, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: detailLabel, attribute: .leading, relatedBy: .equal,
+                               toItem: titleLabel, attribute: .trailing, multiplier: 1, constant: .tbSpacingSmall),
+            NSLayoutConstraint(item: detailLabel, attribute: .trailing, relatedBy: .equal,
+                               toItem: contentView, attribute: .trailing, multiplier: 1, constant: -.tbSpacingCellHorizontalPadding),
+            NSLayoutConstraint(item: detailLabel, attribute: .bottom, relatedBy: .equal,
+                               toItem: contentView, attribute: .bottom, multiplier: 1, constant: 0)
+            ])
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("Do not use this initializer")
+    }
+    
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
     }
 
-    public required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
 }
